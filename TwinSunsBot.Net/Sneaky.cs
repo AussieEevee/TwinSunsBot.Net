c22686a8b0a6d8fc052d47b63e608a4958eb909a
AussieEevee <AussieEevee@outlook.com>
2017-12-16 20:54:17 +0800

    Test

--- TwinSunsBot.Net/Sneaky.cs
+++ TwinSunsBot.Net/Sneaky.cs
@@ -3,7 +3,7 @@
    static public class Sneaky
         {
             private static string token = "MzkxNTcxMTgwMzI4MzIxMDQ0.DRamow.Lj99-sgT5Pn7_nO1FhADBNWoS0c";
-            
+            //test
             public static string Token { get => token; }
         }
     
