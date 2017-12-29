46ca21327a500b5adcd82271e1580e641dba15dc
AussieEevee <AussieEevee@outlook.com>
2017-12-29 23:33:47 +0800

    Clean up

--- TwinSunsBot.Net/Globals.cs
+++ TwinSunsBot.Net/Globals.cs
@@ -10,7 +10,6 @@ namespace TwinSunsBot.Net
     public static class Global
     {
         public static int Number { get; set; }
-        //public static string[] memes = { "Memes/meme01.jpg", "Memes/meme02.jpg", "Memes/meme03.jpg", "Memes/meme04.jpg", "Memes/meme05.jpg", "Memes/meme06.png", "Memes/meme07.jpg", "Memes/meme08.jpg", "Memes/meme09.jpg", "Memes/meme10.jpg", "Memes/meme11.jpg", "Memes/meme12.jpg", "Memes/meme13.jpg", "Memes/meme14.jpg", "Memes/meme15.jpg", "Memes/meme16.jpg", "Memes/meme17.jpg", "Memes/meme18.jpg", "Memes/meme19.jpg", "Memes/meme20.jpg", "Memes/meme21.jpg", "Memes/meme22.jpg", "Memes/meme23.jpg", "Memes/meme24.jpg" };
         public static string[] memes = Directory.GetFiles(@"Memes\");
         public static double version = 1.082;
 
